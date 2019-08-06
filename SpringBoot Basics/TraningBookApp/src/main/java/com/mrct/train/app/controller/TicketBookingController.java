6f17646cb1471ba0235342993ebffdff5d730452
Ravindra babu <33950320+ravindrababuvidya@users.noreply.github.com>
2019-08-06 15:27:46 +0530

    Restful Controller Using Springboot application integrated with JPA using MySQL

--- SpringBoot Basics/TraningBookApp/src/main/java/com/mrct/train/app/controller/TicketBookingController.java
+++ SpringBoot Basics/TraningBookApp/src/main/java/com/mrct/train/app/controller/TicketBookingController.java
@@ -2,11 +2,14 @@ package com.mrct.train.app.controller;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +32,25 @@ public class TicketBookingController {
 	public Ticket getTicketDetails(@PathVariable("ticketId") Integer ticketId) {
 		return ticketBookingService.getTicketDetails(ticketId);
 	}
-
+	
+	@GetMapping(value = "all")
+	public List<Ticket> getAllTickets(){
+		return ticketBookingService.getAllTickets();
+		
+	}
+	
+	@PutMapping(value = { "update/ticketId/{ticketId}/email/{email}",
+			"update/ticketId/{ticketId}/email/{email}/passengerName/{passengerName}" })
+	public Ticket updateTicketDetails(@PathVariable(name = "ticketId", required = true) Integer ticketId,
+			@PathVariable(name = "email", required = false) String email,
+			@PathVariable(name = "passengerName", required = false) String passengerName) {
+		
+		return ticketBookingService.updateTicketDetails(ticketId, email, passengerName);
+	}
+	
+	@DeleteMapping("delete/ticketId/{ticketId}")
+	public String deletTicket(@PathVariable(name = "ticketId") int ticketId) {
+		 ticketBookingService.deletTicket(ticketId);
+		 return "Sucessfully Record Deleted";
+	}
 }
--- SpringBoot Basics/TraningBookApp/src/main/java/com/mrct/train/app/service/TicketBookingService.java
+++ SpringBoot Basics/TraningBookApp/src/main/java/com/mrct/train/app/service/TicketBookingService.java
@@ -2,8 +2,10 @@ package com.mrct.train.app.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.stereotype.Service;
 
 import com.mrct.train.app.dao.TicketBookingDao;
@@ -22,6 +24,36 @@ public class TicketBookingService {
 	public Ticket getTicketDetails(Integer ticketId) {
 		// TODO Auto-generated method stub
 		return ticketBookingDao.findOne(ticketId);
+	}
+
+	public List<Ticket> getAllTickets() {
+		// TODO Auto-generated method stub
+		return (List<Ticket>) ticketBookingDao.findAll();
+	}
+
+	public Ticket updateTicketDetails(Integer ticketId, String email, String passengerName) {
+		// TODO Auto-generated method stub
+		Ticket ticket = ticketBookingDao.findOne(ticketId);
+		if(email == null) {
+			ticket.setEmail(ticket.getEmail());
+		}else {
+			ticket.setEmail(email);
+		}
+		if(passengerName == null) {
+			ticket.setPassengerName(ticket.getPassengerName());
+		}else {
+			ticket.setPassengerName(passengerName);
+		}
+		
+		
+		System.out.println("Updated Ticket Details "+ticketId);
+		return ticketBookingDao.save(ticket);
+	}
+
+	public void deletTicket(int ticketId) {
+		// TODO Auto-generated method stub
+		 ticketBookingDao.delete(ticketId);
+		
 	}
 	
 }
